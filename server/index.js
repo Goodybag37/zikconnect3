1ac333e7c61d80aafe02b815d22eac41eb07ba2e
apple <apple@apples-MacBook-Pro.local>
2024-09-19 20:35:44 +0100

    adding the path

--- server/index.js
+++ server/index.js
@@ -119,6 +119,9 @@ let transporter = nodemailer.createTransport({
   },
 });
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.static(path.join(__dirname, "..client/react-dashboard/build")));
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(fileUpload());
